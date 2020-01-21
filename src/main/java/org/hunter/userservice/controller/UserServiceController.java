a028ccdf53fd887261daf52b02acfa7cbde2cde0
Hunter Stern <vonrosen2000@yahoo.com>
2020-01-20 16:51:03 -0800

    more changes

--- src/main/java/org/hunter/userservice/controller/UserServiceController.java
+++ src/main/java/org/hunter/userservice/controller/UserServiceController.java
@@ -7,6 +7,7 @@ import org.hunter.userservice.model.User;
 import org.hunter.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/user")
 public class UserServiceController {
 
